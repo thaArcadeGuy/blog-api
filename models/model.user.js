613fadc659479be71b8c573d0223c75a9d56efc1
thaArcadeGuy <gilbertsilver64@gmail.com>
2025-06-11 17:22:12 +0300

    feat: add User Model

---
+++ models/model.user.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+});
+
+userSchema.pre("save", async function(next) {
+  if (this.isModified("password")) {
+    const bcrypt = require("bcrypt");
+    const saltRounds = 10;
+    try {
+      const salt = await bcrypt.genSalt(saltRounds);
+      this.password = await bcrypt.hash(this.password, salt);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.passwordIsValid = async function(password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw error
+  }
+};
+
+userSchema.set("toJSON", {
+  transform: function(document, returnedObject) {
+    delete returnedObject.__v;
+    delete returnedObject.password;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
